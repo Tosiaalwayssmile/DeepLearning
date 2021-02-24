c1ab85084f150d7c94aeb7913c211a80322258c0
Antonina Matuszek <37584652+Tosiaalwayssmile@users.noreply.github.com>
2021-02-24 22:51:40 +0100

    Update Ex_6_Broadcasting.py

---
+++ Neural Networks and Deep Learning/Week2/Exercises/Ex_6_Broadcasting.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+"""Calculates the softmax for each row of the input x."""
+def softmax(x):
+  
+    x_exp = np.exp(x)
+
+    x_sum = np.sum(x_exp, axis = 1, keepdims = True)
+    
+    # Compute softmax(x) by dividing x_exp by x_sum. It should automatically use numpy broadcasting.
+    s = x_exp / x_sum
+    
+    return s
+    
+x = np.array([
+    [9, 2, 5, 0, 0],
+    [7, 5, 0, 0 ,0]])
+print("softmax(x) = " + str(softmax(x)))
